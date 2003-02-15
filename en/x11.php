122fcc01e9175b30c8aa8c834414ba5e5d4c84bb
Kevin Van Vechten <kevin@macports.org>
2003-02-15 03:40:48 +0000

    looks prettier before the feedback link.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@2033 d073be05-634f-4543-b044-5fe20cf6d1d6

--- en/x11.php
+++ en/x11.php
@@ -2,7 +2,7 @@
 
 //
 // File     : x11.php
-// Version  : $Id: x11.php,v 1.4 2003/02/15 03:38:15 kevin Exp $
+// Version  : $Id: x11.php,v 1.5 2003/02/15 03:40:48 kevin Exp $
 // Location : /projects/darwinports/en/x11.php
 //
 
@@ -20,11 +20,11 @@ The following X11 packages for Mac OS X were built using the <a href="http://www
 <br><br>
 DarwinPorts is a BSD licensed software build, install, and packaging infrastructure conceived as a part of the OpenDarwin Project. Written almost entirely in TCL, DarwinPorts is designed to be easily extended, embedded in other applications, and ported to a wide variety of architectures. DarwinPorts is in active development and currently supported on Mac OS X 10.2 and Darwin 6.0.2. For more information regarding DarwinPorts, see the <a href="http://www.opendarwin.org/projects/darwinports/">Project Page<a/>
 </p>
-<p align=right><strong>Feedback</strong>:<a href=mailto:dp-feedback@opendarwin.org>dp-feedback@opendarwin.org</a></p>
 <p>
-The DarwinPorts <a href="http://www.opendarwin.org/projects/darwinports/packages
-/Applications/UnInstaller.dmg">UnInstaller</a> may be used to remove these packages from your system.
+The DarwinPorts <a href="http://www.opendarwin.org/projects/darwinports/packages/Applications/UnInstaller.dmg">UnInstaller</a> may be used to remove these packages from your system.
 </p>
+
+<p align="right"><strong>Feedback</strong>:<a href=mailto:dp-feedback@opendarwin.org>dp-feedback@opendarwin.org</a></p>
 
 <p>
 <h4><img align=center src=images/package.jpg>Libraries</h4>
