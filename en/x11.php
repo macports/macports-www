113e79ac4ab8fdb96d96ab2406c4501e92bad986
Kevin Van Vechten <kevin@macports.org>
2003-02-20 04:06:03 +0000

    Deprecated packages.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@2058 d073be05-634f-4543-b044-5fe20cf6d1d6

--- en/x11.php
+++ en/x11.php
@@ -2,7 +2,7 @@
 
 //
 // File     : x11.php
-// Version  : $Id: x11.php,v 1.5 2003/02/15 03:40:48 kevin Exp $
+// Version  : $Id: x11.php,v 1.6 2003/02/20 04:05:59 kevin Exp $
 // Location : /projects/darwinports/en/x11.php
 //
 
@@ -10,136 +10,19 @@
 	od_print_header("X11 Packages for Mac OS X", "en", "iso-8859-1", "", 0, "/projects/darwinports");
 ?>
 
-<!--<center>
-<table border="0" width="500" cellspacing="0" cellpadding="3">
-<tr><td>-->
-<a href=http://www.apple.com/macosx/x11/><img style="float: right;" src=images/x11link.jpg></a>
+<a href="http://www.apple.com/macosx/x11/"><img style="float: right;" src="images/x11link.jpg"></a>
 <h2>X11 Packages for Mac OS X</h2>
 <p>
-The following X11 packages for Mac OS X were built using the <a href="http://www.opendarwin.org/projects/darwinports/">DarwinPorts</a> architecture. Though DarwinPorts installs in /opt/local by default, these packages have been built to install in /usr/local.
-<br><br>
-DarwinPorts is a BSD licensed software build, install, and packaging infrastructure conceived as a part of the OpenDarwin Project. Written almost entirely in TCL, DarwinPorts is designed to be easily extended, embedded in other applications, and ported to a wide variety of architectures. DarwinPorts is in active development and currently supported on Mac OS X 10.2 and Darwin 6.0.2. For more information regarding DarwinPorts, see the <a href="http://www.opendarwin.org/projects/darwinports/">Project Page<a/>
+The X11 packages for Mac OS X previously available here have been removed.  Instead please use the newly released binary packages available via WebDAV at <a href="http://packages.opendarwin.org/">http://packages.opendarwin.org/</a>.  Please view the <a href="http://packages.opendarwin.org/README.txt">README</a> for more information.
 </p>
 <p>
-The DarwinPorts <a href="http://www.opendarwin.org/projects/darwinports/packages/Applications/UnInstaller.dmg">UnInstaller</a> may be used to remove these packages from your system.
+The DarwinPorts <a href="http://packages.opendarwin.org/Applications/UnInstaller.dmg">UnInstaller</a> may be used to remove any packages installed on your system previously downloaded from this page.  You may wish to do this before installing the new packages, since they install into different locations (/opt/local instead of /usr/local).  This UnInstaller is capapble of removing the new packges as well.
 </p>
-
-<p align="right"><strong>Feedback</strong>:<a href=mailto:dp-feedback@opendarwin.org>dp-feedback@opendarwin.org</a></p>
-
-<p>
-<h4><img align=center src=images/package.jpg>Libraries</h4>
-</p>
-
-<p>
-<strong><a name=gtk>Package</a></strong>: gtk
-<br>
-<strong>Version</strong>: 1.2.10
-<br>
-<strong>Description</strong>: GTK+, or the Gimp ToolKit, is a library for GUIs creating for the X Window System.
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/gtk.dmg">gtk.dmg</a>
-</p>
-<hr>
-
-<p>
-<strong>Package</strong>: gtk2 
-<br>
-<strong>Version</strong>: 2.0.9
-<br>
-<strong>Description</strong>: This is GTK+ version 2.0.9. GTK+, or the Gimp ToolKit, is a library for creating GUIs for the X Windows System.
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/gtk2.dmg">gtk2.dmg</a>
-</p>
-<hr>
-<p>
-<strong>Package</strong>: <a name=qt3>qt3<a/>
-<br>
-<strong>Version</strong>: 3.0.5
-<br>
-<strong>Description</strong>: TrollTech's C++ toolkit for writing cross-platform GUI applications.
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/qt3.dmg">qt3.dmg</a>
-</p>
-<hr>
-<p>
-<strong>Package</strong>: openMotif
-<br>
-<strong>Version</strong>: 2.2.2
-<br>
-<strong>Description</strong>: The Open Group's full version of Motif based on the original OSF sources.
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/openMotif.dmg">openMotif.dmg</a>
-</p>
-<hr>
-<p>
-<h4><img align=center src=images/package.jpg>Applications</h4></p>
-<p>
-
-<p>
-<strong>Package</strong>: Gimp
-<br>
-<strong>Version</strong>: 1.2.3
-<br>
-<strong>Description</strong>: The GNU Image Manipulation Program (GIMP) is a powerful tool for the preparation and manipulation of digital images.
-<br>
-<strong>Depends</strong>: <a href=#gtk>gtk</a>
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/gimp.dmg">gimp.dmg</a>
-<br>
-<strong>Note</strong>: To use the gimp, type the following into an xterm: /usr/local/bin/gimp-1.2 --no-shm --no-xshm
-</p>
-<hr>
-<p>
-<strong>Package</strong>: xchat
-<br>
-<strong>Version</strong>: 1.8.10
-<br>
-<strong>Description</strong>: XChat is a feature-rich GTK-based graphical IRC client
-<br>
-<strong>Depends</strong>: <a href=#gtk>gtk</a>
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/xchat.dmg">xchat.dmg</a>
-</p>
-<hr>
-<br>
-<strong>Package</strong>: vim
-<br>
-<strong>Version</strong>: 6.1.271
-<br>
-<strong>Description</strong>: Vim is a virtually compatible, extremely enhanced version of the vi editor.
-<br>
-<strong>Depends</strong>: <a href=#gtk>gtk</a>
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/vim.dmg">vim.dmg</a>
-</p>
-<hr>
-<p>
-<strong>Package</strong>: rxvt
-<br>
-<strong>Version</strong>: 2.7.8
-<br>
-<strong>Description</strong>: Rxvt is an xterm replacement with a smaller memory footprint
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/rxvt.dmg">rxvt.dmg</a>
-<p>
-<hr>
-<br>
-<strong>Package</strong>: qthello
-<br>
-<strong>Version</strong>: 1.0
-<br>
-<strong>Description</strong>: QtHello is an Othello program written in Qt. QtHello currently has 6 different computer players ranging from completely random to tree search with Alpha-Beta pruning.
-<br>
-<strong>Depends</strong>: <a href=#qt3>qt3</a>
-<br>
-<strong>Download</strong>: <a href="http://www.opendarwin.org/projects/darwinports/packages/usr/qthello.dmg">qthello.dmg</a>
-</p>
-
-<!--</td></tr>
-</table>
-</center>-->
-
-
+<br />
+<br />
+<br />
+<br />
+<br />
 <? 
 	od_print_footer("en"); 
 ?>
