4f365dc8b479c1fcb8e68e7a0d37ec5aafd0895a
Matt Anton <matt@macports.org>
2003-11-23 06:14:39 +0000

    Fix utf characters
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@4018 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fr/index.php
+++ fr/index.php
@@ -13,21 +13,21 @@
 	tel que Darwin, Mac OS X, FreeBSD ou Linux.</p>
 	
 	<p>Il n'y a actuellement qu'une centaine de <a href="/fr/ports/">ports</a>
-	op&eacute;rationnels et disponibles, mais d'autres seront bientôt ajout&eacute;s r&eacute;guli&egrave;rement.
+	op&eacute;rationnels et disponibles, mais d'autres seront bient&ocirc;t ajout&eacute;s r&eacute;guli&egrave;rement.
 	Vous pouvez prendre connaissance des ports r&eacute;cemment ajout&eacute;s en souscrivant
 	&agrave; la liste de discussion <a
 	href="http://www.opendarwin.org/mailman/listinfo/cvs-darwinports-all">cvs-darwinports-all</a>
 	ou en utilisant notre <a href="/dp_commits.xml">feed RSS sur les commits r&eacute;cents</a>.</p>
 
 	<p>Pour plus d'information sur l'obtention et l'installation de DarwinPorts,
-	lisez s'il vous plaît la section <a href="/fr/getdp/">R&eacute;cup&eacute;rer DarwinPorts</a>
-	de ce site. Assurez-vous au pr&eacute;alable d'avoir jet&eacute; un œil sur la
+	lisez s'il vous pla&icirc;t la section <a href="/fr/getdp/">R&eacute;cup&eacute;rer DarwinPorts</a>
+	de ce site. Assurez-vous au pr&eacute;alable d'avoir jet&eacute; un oeil sur la
 	<a href="/docs/">documentation</a>; si vous avez des questions ou bien des probl&egrave;mes
 	li&eacute;s &agrave; l'installation/utilisation, vous pouvez vous <a
 	href="/help/">demander de l'aide</a>.</p>
 
 	  <p>Les rapports de bogues, demandes d'ajout de fonctionnalit&eacute;s et les
-	nouveaux ports devront être soumis &agrave; <a href="http://www.opendarwin.org/bugzilla/">Bugzilla</a>.</p>
+	nouveaux ports devront &ecirc;tre soumis &agrave; <a href="http://www.opendarwin.org/bugzilla/">Bugzilla</a>.</p>
 
       <div id="news">
 	<h2 class="hdr">Nouvelles concernant le project</h2>
