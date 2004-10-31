6f17b149e8ce069745a831a2bec12c9d0d331a4f
Matt Anton <matt@macports.org>
2004-10-31 14:09:12 +0000

    Initial check in.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@9039 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ fr/archives.php
@@ -0,0 +1,26 @@
+  <?php
+    $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+    include_once("$DOCUMENT_ROOT/fr/includes/common.inc");
+    include_once("$DOCUMENT_ROOT/fr/includes/functions.inc");
+    print_header('Dépêches DarwinPorts', 'utf-8');
+  ?>
+
+    <div id="content">
+	
+		<h2 class="hdr">Nouvelles archivées</h2>
+     <?php
+		if (isset($_GET['id'])) {
+			print_headline();
+		} else {
+			print_all_headlines_nonadmin();
+		}
+      ?>
+
+
+      </div>
+    </div>
+  </div>
+
+<?php
+  print_footer();
+?>
