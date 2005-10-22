620a93145cb82233a5400aab8c7a424a69bef947
Riccardo Andreoli <riccardo@macports.org>
2005-10-22 11:10:27 +0000

    Initial check in
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@14757 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ it/downloads/index.php
@@ -0,0 +1,32 @@
+  <?php
+    $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+    include_once("$DOCUMENT_ROOT/fr/includes/common.inc");
+    include_once("$DOCUMENT_ROOT/fr/includes/functions.inc");
+    print_header('Distribuzioni di DarwinPorts', 'utf-8');
+  ?>
+
+    <div id="content">
+	
+		<h2 class="hdr">Downloads disponibili</h2>
+		<p>
+<?php
+$dossier = opendir ("$DOCUMENT_ROOT/downloads");
+
+while ($fichier = readdir ($dossier)) {
+	if ($fichier != "." && $fichier != "..") {
+			$tableau = explode (".", $fichier);
+			$nb_element_1 = count ($tableau) -1;
+			if ($tableau[$nb_element_1] != "php") {
+				echo '<a href="/downloads/'.$fichier.'">'.$fichier.'</a><br />';
+			}
+		}
+}
+closedir ($dossier);
+?>
+		</p>
+    </div>
+  </div>
+
+<?php
+  print_footer();
+?>
