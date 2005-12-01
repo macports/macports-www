29e6b275105e788aeb2cd2529a52f828ea5848ee
Matt Anton <matt@macports.org>
2005-12-01 01:34:41 +0000

    Fix a php typo.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@15250 d073be05-634f-4543-b044-5fe20cf6d1d6

--- downloads/index.php
+++ downloads/index.php
@@ -14,7 +14,7 @@ $chemin=".";
 $rep=opendir($chemin);
 chdir($chemin);
 while($file = readdir($rep)) {
-	if($file != '..' && $file !='.' && (!eregi('php$', $file)) {
+	if($file != '..' && $file !='.' && (!eregi('php$', $file))) {
 		if(!is_dir($file)) {
 			echo "<a href=\"/downloads/$file\">$file</a>";
 			echo "<br>";
--- fr/downloads/index.php
+++ fr/downloads/index.php
@@ -14,7 +14,7 @@ $chemin="$DOCUMENT_ROOT/downloads";
 $rep=opendir($chemin);
 chdir($chemin);
 while($file = readdir($rep)) {
-	if($file != '..' && $file !='.' && (!eregi('php$', $file)) {
+	if($file != '..' && $file !='.' && (!eregi('php$', $file))) {
 		if(!is_dir($file)) {
 			echo "<a href=\"/downloads/$file\">$file</a>";
 			echo "<br>";
