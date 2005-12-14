5c5febe985bfc9d750f1584e8859888cd4785796
Matt Anton <matt@macports.org>
2005-12-14 21:40:42 +0000

    Fix a typo.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@15519 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fr/index.php
+++ fr/index.php
@@ -26,7 +26,7 @@
 	</p>
 	
 	<p>
-	Il y a actuellement <?= $count; ?> <a href="/fr/ports/">ports</a>opérationnels
+	Il y a actuellement <?= $count; ?> <a href="/fr/ports/">ports</a> opérationnels
 	et disponibles, mais d'autres seront bientôt ajoutés régulièrement. Vous pouvez
 	prendre connaissance des ports récemment ajoutés en souscrivant à la liste de
 	discussion <a href="http://www.opendarwin.org/mailman/listinfo/cvs-darwinports-all">
