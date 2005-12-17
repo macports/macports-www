02ff7c7969a37d42b761829b2f5de4229c91271b
Riccardo Andreoli <riccardo@macports.org>
2005-12-17 16:34:35 +0000

    Updated for DP 1.2
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@15542 d073be05-634f-4543-b044-5fe20cf6d1d6

--- it/getdp/index.php
+++ it/getdp/index.php
@@ -8,27 +8,26 @@
     <div id="content">
       <h2 class="hdr">Ottenere DarwinPorts</h2>
 
-      <p>DarwinPorts 1.1 è disponibile sia in codice sorgente sia in binario
-	rispettivamente tramite gli archivi
-	<a href="/downloads/DarwinPorts-1.1.tar.bz2">tar.bz2</a>
-	o <a href="/downloads/DarwinPorts-1.1.tar.gz">tar.gz</a> e attraverso
-	il formato <a href="/downloads/DarwinPorts-1.1.dmg">dmg</a> 
-	(Disk Image), contenente un installer pkg grafico. I checksums dei 
+      <p>DarwinPorts 1.2 è disponibile sia in codice sorgente, attraverso
+	il pacchetto <a href="/downloads/DarwinPorts-1.2.tar.bz2">tar.bz2</a>
+	o <a href="/downloads/DarwinPorts-1.2.tar.gz">tar.gz</a>,
+	sia in formato binario dmg (Disk Image), tramite i pacchetti
+	<a href="/downloads/DarwinPorts-1.2-10.4.dmg">dmg per Tiger</a> e
+	<a href="/downloads/DarwinPorts-1.2-10.3.dmg">dmg per Panther</a>,
+	contenenti entrambi un installer pkg grafico. I checksums di tutti i
 	files sopra elencati sono contenuti in 
-	<a href="/downloads/DarwinPorts-1.1.chk.txt">questo file</a>.
-	Attualmente il disk-image è disponibile solo per Mac OS X 10.4/ppc e
-	potrebbe non funzionare correttamente in altre versioni; si consiglia
-	perciò l'installazione da sorgenti.</p>
+	<a href="/downloads/DarwinPorts-1.2.chk.txt">questo file</a>.</p>
 
       <p>Nella <a href="/it/downloads/">sezione downloads</a> puoi trovare la
-	lista completa di tutti i files scaricabili.</p>
+	lista completa di tutti i nostri files scaricabili.</p>
 
-      <p>Ti ricordiamo che, qualora volessi installare ed eseguire DarwinPorts
-	su Mac OS X, devi assicurarti prima di tutto di aver già installato i
-	Mac OS X Developer Tools (per 10.2.x) o XCode (per 10.3.x e 10.4.x).</p>
+      <p>Ti ricordiamo che per installare ed eseguire DarwinPorts su Mac OS X
+	è necessario aver già installato Apple XCode nel proprio sistema; XCode
+	è disponibile sul sito <a href="http://developer.apple.com/">Apple
+	Developer</a> oppure nei CD/DVD di installazione di Mac OS X.</p>
 
       <p>Se invece vuoi utilizzare DarwinPorts su una piattaforma differente
-	da Mac OS X, assicurati di risolvere le seguenti dipendenze:
+	da Mac OS X, assicurati di avere i seguenti requisiti software:
 	  <ul>
 		<li>TCL (8.3 o 8.4)</li>
 		<li>curl</li>
@@ -39,17 +38,16 @@
 
       <p>Il modo più semplice per installare DarwinPorts su piattaforma Mac OS
 	X è scaricare e montare il file
-	<a href="/downloads/DarwinPorts-1.1.dmg">dmg</a> e successivamente
-	lanciare l'applicazione Installer.app contenuta nell'immagine, seguendo
-	le istruzioni che appariranno sullo schermo. Al termine di questa
-	procedura DarwinPorts sarà perfettamente installato nel sistema e
-	pronto all'uso.</p>
-
-      <p>Per utilizzare al meglio DarwinPorts dovrai modificare il file
-	personale di configurazione della shell, in modo tale da poter lanciare
-	i binari installati da qualunque directory. Ti invitiamo a leggere il
-	file README.it comtenuto all'interno del file dmg per portare a termine
-	questa piccola modifica.</p>
+	<a href="/downloads/DarwinPorts-1.2-10.4.dmg">dmg per Tiger</a> o il
+	<a href="/downloads/DarwinPorts-1.2-10.4.dmg">dmg per Panther</a> e
+	successivamente lanciare l'applicazione Installer.app contenuta
+	nell'immagine, seguendo le istruzioni che appariranno sullo schermo.
+	Al termine di questa procedura DarwinPorts sarà perfettamente 
+	installato nel sistema e pronto all'uso. Se sarà necessario, il file
+	personale di configurazione della shell verrà modificato in modo tale
+	da aggiungere tutte le impostazioni indispensabili al corretto
+	funzionamento di DarwinPorts. Dovrai solamente aprire una nuova shell
+	per far sì che queste modifiche abbiano effetto.</p>
 
       <p>Anche se non strettamente necessario, è sempre buona regola
 	sincronizzare la recente installazione con i server di OpenDarwin, così
@@ -69,27 +67,27 @@
       <p>Anche l'installazione da sorgenti richiede pochi e piccoli passi.
 	Una volta scaricato il tarball, da shell, posizionati nella directory
 	contentente il pacchetto ed esegui
-	“<kbd>tar xjvf <a href="/downloads/DarwinPorts-1.1.tar.bz2">
-	DarwinPorts-1.1.tar.bz2</a></kbd>” se hai scelto il tarball bz2, oppure
-	“<kbd>tar xzvf <a href="/downloads/DarwinPorts-1.1.tar.gz">
-	DarwinPorts-1.1.tar.gz</a></kbd>” se invece hai scelto il tarball gz.
+	“<kbd>tar xjvf <a href="/downloads/DarwinPorts-1.2.tar.bz2">
+	DarwinPorts-1.2.tar.bz2</a></kbd>” se hai scelto il tarball bz2, oppure
+	“<kbd>tar xzvf <a href="/downloads/DarwinPorts-1.2.tar.gz">
+	DarwinPorts-1.2.tar.gz</a></kbd>” se invece hai scelto il tarball gz.
 	Quest'ultimo comando estrarrà i sorgenti di DarwinPorts in una nuova
 	subdirectory. Per compilare ed installare DarwinPorts basta eseguire i
 	seguenti comandi:</p>
 
-<pre>cd DarwinPorts-1.1
+<pre>cd DarwinPorts-1.2
 ./configure && make && sudo make install</pre>
 
       <p>Comandi opzionali:</p>
 
 <pre>cd ../
-rm -rf DarwinPorts-1.1.*</pre>
+rm -rf DarwinPorts-1.2.*</pre>
 
       <p>Per effettuare l'installazione sono necessari i privilegi di
 	amministratore, infatti “<kbd>sudo</kbd>” ti chiederà la password
 	per completare il processo. Questa procedura installerà il sistema
 	DarwinPorts e, eseguendo anche i comandi opzionali, rimuoverà tutti
-	gli elementi non più necessari, ossia la directory DarwinPorts-1.1 ed
+	gli elementi non più necessari, ossia la directory DarwinPorts-1.2 ed
 	il tarball corrispondente. Leggendo l'output di
 	“<kbd>./configure --help | more</kbd>” puoi personalizzare
 	l'installazione e passare le opzioni appropriate allo script di
@@ -109,7 +107,7 @@ rm -rf DarwinPorts-1.1.*</pre>
 	installazione.</p>
 
       <p>Alternativamente puoi fare riferimento al file
-	<tt>README_RELEASE1.it</tt> presente nel tarball di release 1.1 per
+	<tt>README_RELEASE1.it</tt> presente nel tarball di release 1.2 per
 	tutto ciò che riguarda l'installazione base e le istruzioni all'uso.</p>
 
       <h5 class="subhdr">Aiuto</h5>
@@ -121,8 +119,8 @@ rm -rf DarwinPorts-1.1.*</pre>
 	ultimissime funzionalità introdotte, puoi acquisire i sorgenti tramite
 	CVS.</p>
 
-      <p>I seguenti comandi effettuano il checkout del progetto dal CVS
-	repository di OpenDarwin:</p>
+      <p>I seguenti comandi effettuano il checkout del progetto dall'
+	anonymous CVS repository di OpenDarwin:</p>
 
 <pre>cvs -d :pserver:anonymous@anoncvs.opendarwin.org:/Volumes/src/cvs/od login
 cvs -d :pserver:anonymous@anoncvs.opendarwin.org:/Volumes/src/cvs/od co -P darwinports</pre>
