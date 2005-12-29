48a6e168978cdddb19197c85888cbc3172acc2e4
Juan Manuel Palacios <jmpp@macports.org>
2005-12-29 15:54:25 +0000

    Submitted by:   jmpp@
    Merge the dp_version function introduced by Matt into the french site.
    Some other rewordings.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@15628 d073be05-634f-4543-b044-5fe20cf6d1d6

--- getdp/index.php
+++ getdp/index.php
@@ -8,13 +8,13 @@
     <div id="content">
       <h2 class="hdr">Get DarwinPorts</h2>
 
-	 <p>DarwinPorts version 1.2 is available in source form as either a
-	 <a href="/downloads/DarwinPorts-1.2.tar.bz2">tar.bz2</a>
-	 package or a <a href="/downloads/DarwinPorts-1.2.tar.gz">tar.gz</a> one,
-	 and in binary form as a <a href="/downloads/DarwinPorts-1.2-10.4.dmg">dmg for Tiger</a>
-	 or as <a href="/downloads/DarwinPorts-1.2-10.3.dmg">one for Panther</a>,
-	 both containing a pkg installer. Checksums for all these are contained
-	 <a href="/downloads/DarwinPorts-1.2.chk.txt">here</a>.</p>
+	 <p>DarwinPorts version <? print "$dp_version"; ?> is available in binary form as a
+	 dmg disk image <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>-10.4.dmg">for Tiger</a>
+	 or <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>-10.3.dmg">for Panther</a>,
+	 both containing a pkg installer, or in source form as either a
+	 <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>.tar.bz2">tar.bz2</a> package
+	 or a <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>.tar.gz">tar.gz</a> one.
+	 Checksums for all these are contained <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>.chk.txt">here</a>.</p>
 
 	 <p>To get a files listing of all our available downloads checkout the
 	 <a href="/downloads/">downloads section</a></p>
@@ -35,18 +35,19 @@
 	<h5 class="subhdr">Mac OS X Package (.pkg) Installer</h5>
 
 	<p>The easiest way to install DarwinPorts on a Mac OS X system is by downloading
-	the <a href="/downloads/DarwinPorts-1.2-10.4.dmg">dmg for Tiger</a> or the
-	<a href="/downloads/DarwinPorts-1.2-10.3.dmg">one for Panther</a> and running Installer.app
-	on the pkg contained therein, following the on-screen instructions until completion.
-	This procedure will place a fully functional and default DarwinPorts installation
-	on your host system, completely ready for usage. If needed, your shell configuration
-	files will be adapted by the installer to include the necessary settings to run
-	DarwinPorts. You may need to open a new shell for these changes to take effect.</p>
+	the <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>-10.4.dmg">dmg for Tiger</a>
+	or the <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>-10.3.dmg">one for Panther</a>
+	and running Installer.app on the pkg contained therein by double clicking on them,
+	following the on-screen instructions until completion. This procedure will place a
+	fully functional and default DarwinPorts installation on your host system,
+	ready for usage. If needed, your shell configuration files will be adapted by the installer
+	to include the necessary settings to run DarwinPorts. You may need to open a new shell for
+	these changes to take effect.</p>
 
 	<p>Although not strictly necessary, it is still recommended that you synchronize your
-	recent installation with OpenDarwin servers to ensure you have the latest release
-	available of the DarwinPorts infrastructure and of the Portfiles containing the instructions
-	necessary for the installation of ports. To accomplish this simply execute:</p>
+	recent installation with our rsync server to ensure you have the latest release
+	available of the DarwinPorts infrastructure and of the &ldquo;Portfiles&rdquo; containing the instructions
+	employed in the building and installation of ports. To accomplish this simply execute:</p>
 
 	<pre>sudo port -d selfupdate</pre>
 
@@ -55,27 +56,28 @@
 
 	<h5 class="subhdr">Source Installation</h5>
 
-	<p>If on the other hand you decide to install from source, there are still a few
+	<p>If on the other hand you decide to install from source, there are still a couple of
 	things you will need to do once you've downloaded the tarball before you can
-	install a port with the DarwinPorts system, namely installing DarwinPorts itself.
+	install a port with the DarwinPorts system, namely compiling and installing DarwinPorts itself.
 	&ldquo;<kbd>cd</kbd>&rdquo; into the directory where you downloaded the package
-	and run &ldquo;<kbd>tar xjvf <a href="/downloads/DarwinPorts-1.2.tar.bz2">DarwinPorts-1.2.tar.bz2</a></kbd>&rdquo;
-	or &ldquo;<kbd>tar xzvf <a href="/downloads/DarwinPorts-1.2.tar.gz">DarwinPorts-1.2.tar.gz</a></kbd>&rdquo;,
+	and run &ldquo;<kbd>tar xjvf <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>.tar.bz2">DarwinPorts-<? print "$dp_version"; ?>.tar.bz2</a></kbd>&rdquo;
+	or &ldquo;<kbd>tar xzvf <a href="/downloads/DarwinPorts-<? print "$dp_version"; ?>.tar.gz">DarwinPorts-<? print "$dp_version"; ?>.tar.gz</a></kbd>&rdquo;,
 	depending on whether you downloaded the bz2 tarball or the gz one, respectively.
 	This will unpack the DarwinPorts sources that you will proceed to build and install. To do
 	so, execute the following:</p>
 
-      <pre>cd DarwinPorts-1.2
+      <pre>cd DarwinPorts-<? print "$dp_version"; ?>
+
 ./configure && make && sudo make install</pre>
 
 	<p>Optionally:</p>
 
 <pre>cd ../
-rm -rf DarwinPorts-1.2.*</pre>
+rm -rf DarwinPorts-<? print "$dp_version"; ?>*</pre>
 
       <p>These steps need to be perfomed from an administrator account, for which &ldquo;<kbd>sudo</kbd>&rdquo;
       will ask the password upon installation. This procedure will install a pristine DarwinPorts
-      system and, if the optional steps are taken, remove the as of now unnecessary DarwinPorts-1.2
+      system and, if the optional steps are taken, remove the as of now unnecessary DarwinPorts-<? print "$dp_version"; ?>
       directory and corresponding tarball. To customize your installation you should read the output
       of &ldquo;<kbd>./configure --help | more</kbd>&rdquo; and pass the appropriate options to the
       configuration script in the steps detailed above.</p>
@@ -89,7 +91,8 @@ rm -rf DarwinPorts-1.2.*</pre>
 	the above command on a regular basis to keep your installation current.</p>
 
       <p>Alternatively, you can refer to the <tt>README_RELEASE1</tt>
-	file contained in the 1.2 release tarball for basic installation and usage instructions.</p>
+	file contained in the <? print "$dp_version"; ?> release tarball for basic installation and usage
+	instructions.</p>
 
 	<h5 class="subhdr">Help</h5>
 
--- help/index.php
+++ help/index.php
@@ -13,15 +13,13 @@
 	  
 		<h5 class="subhdr">Documentation</h5>
 
-		<p>The <a href="http://www.darwinports.org/docs">DarwinPorts 
-			Guide</a> has a section for DarwinPorts users, 
-			<a href="http://www.darwinports.org/docs/pt01.html">Part 
-			1: Using DarwinPorts</a>.</p>
+		<p>The <a href="/docs/">DarwinPorts Guide</a> has a section for DarwinPorts users, 
+		<a href="http://www.darwinports.org/docs/pt01.html">Part 1: Using DarwinPorts</a>.</p>
 
 		<p>The <a href="http://wiki.opendarwin.org/index.php/DarwinPorts:FAQ">DarwinPorts FAQ</a>
 		is now an ongoing, user driven effort part of our <a href="http://wiki.opendarwin.org/index.php/DarwinPorts">Wiki</a>,
-		where anyone with a Wiki account and DarwinPorts knowledge can contribute with
-		information to help others.</p>
+		where anyone with a <a href="http://wiki.opendarwin.org/index.php?title=Special:Userlogin&returnto=DarwinPorts">Wiki account</a>
+		and DarwinPorts knowledge can contribute with information to help others.</p>
 
 		<p>All of our documentation is a work in progress, so if you spot 
 			an error, or have a quesiton about some part of the document, 
@@ -30,47 +28,43 @@
 		<h5 class="subhdr">Mailing Lists</h5>
 
 		<p>The 	
-			<a href="http://www.opendarwin.org/mailman/listinfo/darwinports">darwinports 
+			<a href="http://www.opendarwin.org/mailman/listinfo/darwinports">General DarwinPorts 
 			mailing list</a> is open to all.  It is the best place to ask 
 			questions about DarwinPorts, for new users, developers, 
 			everyone alike!  It is also where all discussion about 
-			DarwinPorts takes place.  Please note that due to spam problems,
+			DarwinPorts itself takes place.  Please note that due to spam problems,
 			the DarwinPorts mailing list requires posts from 
-			non-subscribers to be approved.  It may be better to subscribe 
-			to the list.</p>
+			non-subscribers to be approved.  It may be better to susbcribe.</p>
 
-		<p>You may check the 
-			<a href="http://www.opendarwin.org/pipermail/darwinports/">darwinports 
-			mailing list archives</a> before posting a question, although 
-			it's not necessarily a requirement.  We try to be as helpful as 
-			possible, but if it's a common question, our answers may be 
-			fairly short.</p>
+		<p>Although not strictly a requirement, you may check the 
+			<a href="http://www.opendarwin.org/pipermail/darwinports/">list archives</a>
+			before posting a question. We try to be as helpful as possible, but
+			if it's a common question our answers may be fairly short.</p>
 
-		<p>Otherwise, archives of the darwinports mailing list are easily browsable at 
+		<p>Otherwise, indexed archives of this list are easily browsable at 
 			<a href="http://gmane.org/">Gmane.org</a> as well as through 
 			<a href="nntp://news.gmane.org/">NNTP</a>.</p>
 
 		<p>When you post a question to the mailing list, please included 
 			any information you think might be relevent to the problem, 
-			such as what operating system and version you're using (OS X 
-			10.3.2, for example), whether you have any other third party 
-			software installed /usr/local (for example), and any error 
+			such as what operating system and version you're using, Mac OS X 
+			10.3.2 for example, whether you have any other third party 
+			software installed, in /usr/local for instance, and any error 
 			messages that DarwinPorts might give (use the -d and -v port(1)'s 
-			flags to turn on debugging informations). It's a lot easier for 
-			us to help you once these flags are used.</p>
+			flags to turn on debugging informations, it's a lot easier for 
+			us to help you once these are used).</p>
 
 		<h5 class="subhdr">IRC</h5>
 
 		<p>For more real-time discussion, the #darwinports channel on the <a
-			href="http://freenode.net/">freenode IRC network</a> is generally
+			href="http://freenode.net/">Freenode IRC network</a> is generally
 			where we hang out.</p>
 	
 		<p>Though it is generally helpful, please keep in mind that no one is
 			obligated to help or even answer your question if you join the IRC
 			channel.  Do not take it personally, simply ask your question
-			on the
-		   	<a href="http://www.opendarwin.org/mailman/listinfo/darwinports">darwinports
-			mailing list</a> instead.</p>
+			on the <a href="http://www.opendarwin.org/mailman/listinfo/darwinports">mailing list</a>
+			instead.</p>
 
 	</div>
 </div>
