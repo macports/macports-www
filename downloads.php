a070bba339ab0e1520cae67e14587a5dad4d78f9
Juan Manuel Palacios <jmpp@macports.org>
2007-09-11 20:18:49 +0000

    Whitespace changes only: 4-space soft-tabs.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@28922 d073be05-634f-4543-b044-5fe20cf6d1d6

--- downloads.php
+++ downloads.php
@@ -1,43 +1,38 @@
-  <?php
+<?php
     /* $Id$ */
     $MPWEB = $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['SCRIPT_NAME']);
     include_once("$MPWEB/includes/common.inc");
     print_header('MacPorts -- Downloads', 'utf-8');
-  ?>
+?>
 
     <div id="content">
-		<h2 class="hdr">Available Downloads</h2>
-		<table>
+        <h2 class="hdr">Available Downloads</h2>
+        <table>
 <?php
-$dir=".";
+            $dir=".";
+            // Read files from the dirctory
+            $files = array();
+            $rep=opendir($dir);
+            while ($file = readdir($rep)) {
+                if (preg_match('/^(\..*)|(.*\.(php|css|html|js))$/', $file) || is_dir($file)) { continue; }
+                // Add file to array, as key, with modtime as value
+                $files[$file] = filemtime($file);
+            }
+            closedir($rep);
 
-// Read files from the dirctory
-$files = array();
-$rep=opendir($dir);
-while ($file = readdir($rep)) {
-	if (preg_match('/^(\..*)|(.*\.(php|css|html|js))$/', $file)
-		|| is_dir($file)
-	   ) {
-		continue;
-	}
-	
-	// Add file to array, as key, with modtime as value
-	$files[$file] = filemtime($file);
-}
-closedir($rep);
+            // Sort the array in reverse order by value (modtime)
+            arsort($files);
 
-// Sort the array in reverse order by value (modtime)
-arsort($files);
-
-// Emit the files, with dates
-foreach ($files as $f => $t) {
-	echo "<tr>";
-	echo "<td>".date("d-M-Y G:i", $t)."</td><td><a href=\"/macports/downloads/$f\">$f</a></td>\n";
-	echo "</tr>";
-}
+            // Emit the files, with dates
+            foreach ($files as $f => $t) {
+                echo "<tr>";
+                echo "<td>".date("d-M-Y G:i", $t)."</td><td><a href=\"/macports/downloads/$f\">$f</a></td>\n";
+                echo "</tr>";
+            }
 ?>
-		</table>
+        </table>
     </div>
+
 <?php
-  print_footer();
+    print_footer();
 ?>
