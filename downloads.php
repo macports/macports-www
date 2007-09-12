6645c296626ed77d1ecb266196ad39236e5b056c
Juan Manuel Palacios <jmpp@macports.org>
2007-09-12 16:57:04 +0000

    Refactor the downloads.php file to be xhtml complaint even if there are no available files. That is, check the $files array's lenght and don't print the table if 0.
    Also clean up whitespace... sorry for the mixed commit!
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@28943 d073be05-634f-4543-b044-5fe20cf6d1d6

--- downloads.php
+++ downloads.php
@@ -5,33 +5,37 @@
     print_header('MacPorts -- Downloads', 'utf-8');
 ?>
 
-    <div id="content">
-        <h2 class="hdr">Available Downloads</h2>
-        <table>
+<div id="content">
+    <h2 class="hdr">Available Downloads</h2>
 <?php
-            $dir=".";
-            // Read files from the dirctory
-            $files = array();
-            $rep=opendir($dir);
-            while ($file = readdir($rep)) {
-                if (preg_match('/^(\..*)|(.*\.(php|css|html|js))$/', $file) || is_dir($file)) { continue; }
-                // Add file to array, as key, with modtime as value
-                $files[$file] = filemtime($file);
-            }
-            closedir($rep);
+    $dir=".";
+    // Read files from the dirctory
+    $files = array();
+    $rep=opendir($dir);
+    while ($file = readdir($rep)) {
+        if (preg_match('/^(\..*)|(.*\.(php|css|html|js))$/', $file) || is_dir($file)) {
+            continue;
+        }
+        // Add file to array, as key, with modtime as value
+        $files[$file] = filemtime($file);
+    }
+    closedir($rep);
 
-            // Sort the array in reverse order by value (modtime)
-            arsort($files);
-
-            // Emit the files, with dates
-            foreach ($files as $f => $t) {
-                echo "<tr>";
-                echo "<td>".date("d-M-Y G:i", $t)."</td><td><a href=\"/macports/downloads/$f\">$f</a></td>\n";
-                echo "</tr>";
-            }
+    // Print the table only if the $files array is non-emtpy
+    if (count($files)) {
+        // Sort the array in reverse order by value (modtime)
+        arsort($files);
+        // Emit the files, with dates
+        echo "<table>";
+        foreach ($files as $f => $t) {
+            echo "<tr>";
+            echo "<td>".date("d-M-Y G:i", $t)."</td><td><a href=\"downloads/$f\">$f</a></td>\n";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
 ?>
-        </table>
-    </div>
+</div>
 
 <?php
     print_footer();
