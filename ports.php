8a0157cd496121251c414ef241615d7f01418cad
Juan Manuel Palacios <jmpp@macports.org>
2007-10-19 01:40:48 +0000

    Follow-up to https://trac.macports.org/changeset/30043 and include Copyright header.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@30046 d073be05-634f-4543-b044-5fe20cf6d1d6

--- ports.php
+++ ports.php
@@ -1,6 +1,8 @@
 <?php
     /* -*- coding: utf-8; mode: php; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:filetype=php:et:sw=4:ts=4:sts=4 */
     /* $Id$ */
+    /* Copyright (c) 2004-2007, MacPorts Organization */
+    /* Copyright (c) 2004, OpenDarwin */
     $MPWEB = $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['SCRIPT_NAME']);
     include_once("$MPWEB/includes/common.inc");
     include_once("$MPWEB/includes/email.inc");
