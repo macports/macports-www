871444a35fecef5f0d49e5774e0e47ba1eb2dddd
Juan Manuel Palacios <jmpp@macports.org>
2007-11-19 21:03:54 +0000

    Finally publish some of the (unfortunately still too incipient) ideas I have for our new main page, so that others can take a look at what I'm doing. If you, reader, have any feedback, please don't feel shy to chime in, I'm advancing a tad too slow on this single remaining page!
    Also copy the old page into index_old.php, so that we can compare new against old side to side.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@31308 d073be05-634f-4543-b044-5fe20cf6d1d6

--- index.php
+++ index.php
@@ -1,58 +1,44 @@
 <?php
     /* -*- coding: utf-8; mode: php; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:filetype=php:et:sw=4:ts=4:sts=4 */
+    /* Copyright (c) 2007, The MacPorts Project. */
     /* $Id$ */
     $MPWEB = $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['SCRIPT_NAME']);
     include_once("$MPWEB/includes/common.inc");
-    print_header('MacPorts -- Home', 'utf-8');
+    print_header('The MacPorts Project -- Home', 'utf-8');
 ?>
 
-<!--
-Things we should address when rewriting this page:
- *) project description (general information, including project license & latest available release);
- *) mission goals;
- *) brief ports tree description (monolithic, no 'stable' and 'unstable' trees, for instance) and total number of available ports, cue to 'ports.php';
- *) system requirements, cue to 'install.php';
- *) FAQ reference as a cue to "Support & Development" section & project guide;
- *) how to contribute and/or join, cue to 'contact.php';
- *) News section, linking to wordpress blog;
- *) something else?
--->
 
 <div id="content">
 
-    <h2 class="hdr">Introduction to MacPorts</h2>
+    <h2 class="hdr">The MacPorts Project Homepage</h2>
 
-    <p>The MacPorts Project's main goal is to provide an easy way to install various open-source software
-    products on the <a href="http://www.apple.com/macosx/">Mac OS X</a> operating system.</p>
-
-    <p>There are currently about <?php echo ports_count(); ?> completed and usable <a href="ports.php">ports</a>,
-    with more being added on a regular basis. You can track recently added ports by subscribing to the
-    <a href="http://lists.macosforge.org/mailman/listinfo/macports-changes">macports-changes</a> mailing list.</p>
-
-    <p>For more information on obtaining and installing MacPorts, please see the <a href="install.php">installing
-    MacPorts</a> section of this site. Also be sure to check out the <a href="<?php print $guide_url; ?>">documentation</a>,
-    and if and if you have questions or run into problems, you can get help at our <a href="help.php">help section</a>.
-    The <a href="<?php print $trac_url . 'wiki'; ?>">MacPorts Wiki</a> is also a good resource for general and
-    miscellaneous help, specially the ongoing <a href="<?php print $trac_url . 'wiki/FAQ'; ?>">FAQ</a>
-    effort.</p>
-
-    <p>Bug reports, feature requests, and new ports should be submitted as <a href="<?php print $trac_url . 'newticket'; ?>">
-    new tickets</a> into our Trac system. Please consult the <a href="<?php print $guide_url . '#project.tickets'; ?>">
-    documentation</a> to improve the processing of your ticket(s).</p>
-
-    <p>For information on becoming an official member of the MacPorts project with write access to our subversion
-    repository, please consult the <a href="<?php print $guide_url . '#project.membership'; ?>">relevant documentation</a>
-    detailing our membership requirements and resulting duties &amp; benefits from holding a MacPorts account.</p>
-
+    <!-- brief project description: general information, project license, mission goals -->
+    <p>The MacPorts Project is an open-source community initiative to design an easy-to-use system for compiling, installing,
+    and upgrading open-source software on the <a href="http://www.apple.com/macosx/">Mac OS X</a> operating system. To that
+    end we provide the <a href="install.php">MacPorts software package</a> under a <a href="http://opensource.org/licenses/bsd-license.php">
+    BSD License</a>, and through it easy access to thousands of <a href="<?php print $guide_url . '#introduction'; ?>">ports</a>
+    that greatly simplify the task of compiling and installing open source software on your Mac.</p>
+    
+    <!-- brief ports tree description: monolithic, no 'stable' and 'unstable' trees; total number of available ports -->
+    <p>We provide a single <a href="ports.php">software tree</a> that attempts to track the latest release of every software
+    title (port) we distribute, without splitting them into &ldquo;stable&rdquo; Vs. &ldquo;unstable&rdquo; categories. There
+    are currently <b><?php print ports_count(); ?></b> ports in it and more are being added on a regular basis.</p>
+    
+    <!-- latest available release & cue to install.php -->
     <p><b>Current MacPorts <a href="install.php">release</a>: <?php print $mp_version_latest; ?></b></p>
 
-    <div id="news">
+    <!-- Cue to "Support & Development" section & project guide -->
+    <p>problems with installing and using MacPorts, project guide...</p>
+    
+    <!-- how to contribute and/or join, cue to 'contact.php' -->
+    <p>if you are interested in participating/joining in any way...</p>
 
-        <h2 class="hdr">Project News &amp; Blog</h2>
-
-    </div>
+    <!-- News & blog section -->
+    <h2 class="hdr">Project News &amp; Blog</h2>
+    
 
 </div>
+
 
 <?php
     print_footer();
