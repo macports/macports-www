4b044e9d92a5328a44a143828a38a1ca7f8e8c34
William Siegrist <wsiegrist@apple.com>
2008-07-31 20:20:25 +0000

    Adding docs index to help track and consolidate our documentation down to a more reasonable list.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@38801 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ docs.php
@@ -0,0 +1,67 @@
+<?php
+    /* -*- coding: utf-8; mode: php; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:filetype=php:et:sw=4:ts=4:sts=4 */
+    /* $Id:$ */
+    /* Copyright (c) 2008, The MacPorts Project. */
+    $MPWEB = $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['SCRIPT_NAME']);
+    include_once("$MPWEB/includes/common.inc");
+    print_header('The MacPorts Project -- Documentation', 'utf-8');
+?>
+
+
+<div id="content">
+
+    <h2 class="hdr">Documentation</h2>
+
+    <br/>
+
+    <h3 class="subhdr" id="installation">Installation</h3>
+    	<ul>
+		<li><a href="http://www.macports.org/install.php">Installation</a></li>
+		<li><a href="http://guide.macports.org/#installing">Guide to Installation</a></li>
+		<li><a href="http://trac.macports.org/wiki/InstallingMacPorts">Installation Wiki</a></li>
+		<li><a href="http://trac.macports.org/wiki/howto/RunningTrunk">Installing from SVN Trunk</a></li>
+		<li><a href="http://trac.macports.org/wiki/GetMacPortsSource">Get the Source</a></li>
+	</ul>
+
+    <h3 class="subhdr" id="howtos">HOWTOs</h3>
+    	<ul>
+		<li><a href="http://trac.macports.org/wiki/howto">HOWTO Wiki</a></li>
+	</ul>
+    
+    <h3 class="subhdr" id="guide">Guide</h3>
+    	<ul>
+		<li><a href="http://guide.macports.org/">Single HTML</a></li>
+	</ul>    
+
+    <h3 class="subhdr" id="faq">FAQs</h3>
+    	<ul>
+		<li><a href="http://trac.macports.org/wiki/FAQ">FAQ Wiki</a></li>
+		<li><a href="http://trac.macports.org/wiki/ProblemHotlist">Problem Hotlist</a></li>
+		<li><a href="http://trac.macports.org/wiki/LeopardProblems">Leopard Problems</a></li>
+	</ul>
+    
+    <h3 class="subhdr" id="bugreports">Bug Reports</h3>
+    	<ul>
+		<li><a href="http://guide.macports.org/#project">Reporting Guidelines</a></li>
+		<li><a href="http://trac.macports.org/wiki/TracTicketing">Ticket Reporting Wiki</a></li>
+	</ul>
+    
+    <h3 class="subhdr" id="membership">Membership</h3>
+    	<ul>
+		<li><a href="http://guide.macports.org/#project.membership">Project Membership Guide</a></li>
+		<li><a href="http://trac.macports.org/wiki/NewCommittersGuide">New Committers Guide</a></li>
+		<li><a href="http://trac.macports.org/wiki/CommittersTipsAndTricks">Tips and Tricks</a></li>
+	</ul>
+
+    <h3 class="subhdr" id="usage">How to Use MacPorts</h3>
+    	<ul>
+		<li><a href="http://guide.macports.org/#using">Using Macports</a></li>
+		<li><a href="http://trac.macports.org/wiki/UsingMacPortsQuickStart">Quick Start</a></li>
+	</ul>
+    
+</div>
+
+
+<?php
+  print_footer();
+?>
