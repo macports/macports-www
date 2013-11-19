b04863ff1bd2c02a4f35ff66512374fa1b194fb8
Jeremy Lavergne <snc@macports.org>
2013-11-19 22:21:43 +0000

    www: revert https://trac.macports.org/changeset/113550, https://trac.macports.org/changeset/113549 see https://lists.macosforge.org/pipermail/macports-dev/2013-November/025199.html
    git-svn-id: https://svn.macports.org/repository/macports/trunk/www@113562 d073be05-634f-4543-b044-5fe20cf6d1d6

--- install.php
+++ install.php
@@ -13,12 +13,7 @@
     <h2 class="hdr" id="quickstart">Quickstart</h2>
 
     <ol>
-        <li>Install Xcode
-            <ul>
-                <li>Mavericks: In Terminal, use <code>xcode-select --install</code></li>
-                <li>Others see <a href="http://guide.macports.org/#installing.xcode">Xcode and the Xcode Command Line Tools</a></li>
-            </ul>
-        </li>
+        <li>Install <a href="http://guide.macports.org/#installing.xcode">Xcode and the Xcode Command Line Tools</a></li>
         <li>Agree to Xcode license in Terminal: <code>sudo xcodebuild --license</code></li>
         <li>Install MacPorts for your version of OS X:
             <ul>
