86c06a48d41cfa0c5cf5282396bd6e2b54bc23fe
Eitan Adler <lists@eitanadler.com>
2017-12-29 12:14:25 -0800

    language.js: Lint Javascript

--- language.js
+++ language.js
@@ -1,9 +1,10 @@
-window.addEvent('domready', function(){
-    var langSlider = new Fx.Slide('languages', {mode:'horizontal'});
+"use strict";
+window.addEvent("domready", function() {
+    var langSlider = new Fx.Slide("languages", {mode: "horizontal"});
     langSlider.hide();
-    $('language').addEvent('click', function(e){
-            e = new Event(e);
-            langSlider.toggle();
-            e.stop();
-        });
+    $("language").addEvent("click", function (e) {
+        e = new Event(e);
+        langSlider.toggle();
+        e.stop();
+    });
 });
