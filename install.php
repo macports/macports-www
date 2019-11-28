22fef4b7fa0fa05191c871b6a4ac8caeeee7733b
Joshua Root <jmr@macports.org>
2019-11-28 20:11:49 +1100

    adjust wording

--- install.php
+++ install.php
@@ -58,7 +58,7 @@
     <p>Checksums for our packaged <a href="<?php print $downloads; ?>">downloads</a> are contained in the corresponding <a
     href="<?php print $checksums; ?>">checksums file</a>.</p>
     
-    <p>The public key to verify package signatures can be found <a href="https://trac.macports.org/wiki/jmr#no1">under the attachments section on the jmr's wiki page</a>. (<a href="https://trac.macports.org/attachment/wiki/jmr/jmr_at_macports_org-2013.pubkey">Direct Link</a>).</p>
+    <p>The public key to verify the detached GPG signatures can be found under the <a href="https://trac.macports.org/wiki/jmr#no1">attachments section on jmr's wiki page</a>. (<a href="https://trac.macports.org/attachment/wiki/jmr/jmr_at_macports_org-2013.pubkey">Direct Link</a>).</p>
 
 
     <div id="requirements">
